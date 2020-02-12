1c1ae7ec641ed120c8e24be649c5ba8733e6ed9d
rexypark <hotboa3091@naver.com>
2020-02-12 17:05:06 +0900

    dgdg

---
+++ 37_SpringMVC_Mybatis_test/src/main/java/com/spring/biz/board/BoardServiceClient.java
@@ -0,0 +1,43 @@
+package com.spring.biz.board;
+
+import java.util.List;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BoardServiceClient {
+
+	public static void main(String[] args) {
+		//1. 스프링컨테이너 구동
+		AbstractApplicationContext container
+			= new GenericXmlApplicationContext("applicationContext.xml");
+		
+		System.out.println("\n--- 컨테이너 구동후 ----");
+		//2. 스프링컨테이너에 생성된 객체 요청 및 사용
+		BoardService boardService
+			= (BoardService) container.getBean("boardService");
+		
+		//2-1. 입력
+		BoardVO vo = new BoardVO();
+		vo.setTitle("테스트-2");
+		vo.setWriter("홍길동2");
+		vo.setContent("테스트-2 내용");
+		
+		
+		//전체목록 조회
+		System.out.println("==============");
+		List<BoardVO> boardList = boardService.getBoardList();
+		for (BoardVO board : boardList) {
+			System.out.println(board);
+		}
+		
+		boardService.insertBoard(vo);
+		
+		
+		
+		//3. 컨테이너 종료(close)
+		container.close();
+
+	}
+
+}
---
+++ 37_SpringMVC_Mybatis_test/src/main/java/com/spring/biz/board/BoardVO.java
@@ -0,0 +1,124 @@
+package com.spring.biz.board;
+
+import java.sql.Date;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/*
+CREATE TABLE BOARD (
+	SEQ NUMBER(5) PRIMARY KEY,
+	TITLE VARCHAR2(200),
+	WRITER VARCHAR2(20),
+	CONTENT VARCHAR2(2000),
+	REGDATE DATE DEFAULT SYSDATE,
+	CNT NUMBER(5) DEFAULT 0
+);
+*/
+public class BoardVO {
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regdate;
+	private int cnt;
+	
+	//검색조건용 필드 추가
+	private String searchCondition;
+	private String searchKeyword;
+	
+	
+	//파일 업로드
+	private MultipartFile uploadFile;
+	
+	
+	public BoardVO() {
+		//System.out.println(">> BaordVO 객체 생성");
+	}
+	
+	
+	//파일 업로드
+	//========================================================
+	@JsonIgnore
+	public MultipartFile getUploadFile() {
+		return uploadFile;
+	}
+	public void setUploadFile(MultipartFile uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+	
+	
+	
+	//검색 관련
+	//========================================================
+	@JsonIgnore
+	public String getSearchCondition() {
+		return searchCondition;
+	}
+	public void setSearchCondition(String searchCondition) {
+		this.searchCondition = searchCondition;
+	}
+	@JsonIgnore
+	public String getSearchKeyword() {
+		return searchKeyword;
+	}
+	public void setSearchKeyword(String searchKeyword) {
+		this.searchKeyword = searchKeyword;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	@Override
+	public String toString() {
+		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regdate="
+				+ regdate + ", cnt=" + cnt + "]";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
